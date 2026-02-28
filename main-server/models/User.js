f8cb20208241befddd50ddb65966c84af34a2f3d
sesh-31 <137743800+sesh-31@users.noreply.github.com>
2026-02-28 12:10:37 +0545

    Rewrite user.js (#4)

--- main-server/models/User.js
+++ main-server/models/User.js
@@ -5,49 +5,61 @@ const User = sequelize.define(
   "User",
   {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.BIGINT,
       primaryKey: true,
       autoIncrement: true,
     },
-
-    username: {
-      type: DataTypes.STRING,
+    firstname_txt: {
+      type: DataTypes.STRING(100),
       allowNull: false,
-      unique: true,
     },
-    email: {
-      type: DataTypes.STRING,
+    lastname_txt: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("SUPERADMIN", "ADMIN", "TEACHER", "STUDENT"),
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING(100),
       allowNull: false,
       unique: true,
     },
     password: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT("long"),
       allowNull: false,
     },
-
-    firstName: {
-      type: DataTypes.STRING,
+    email_txt: {
+      type: DataTypes.STRING(255),
       allowNull: true,
     },
-
-    lastName: {
-      type: DataTypes.STRING,
+    phone_num_txt: {
+      type: DataTypes.STRING(10),
       allowNull: true,
     },
-
-    profilePicture: {
-      type: DataTypes.STRING,
+    stud_center_fk_id: {
+      type: DataTypes.BIGINT,
       allowNull: true,
     },
-
-    profilePicturePublicId: {
-      type: DataTypes.STRING,
+    stud_batch_year: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+    },
+    stud_exam_symbol_no: {
+      type: DataTypes.STRING(100),
+      allowNull: true,
+    },
+    stud_exam_reg_no: {
+      type: DataTypes.STRING(100),
       allowNull: true,
     },
   },
   {
-    tableName: "users",
+    tableName: "User", 
     timestamps: true,
+    createdAt: "createdAt_ts",
+    updatedAt: "updatedAt_ts",
   }
 );
 
